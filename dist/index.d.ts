2c2a260fca15b0255df9208cef3f0b5957b20097
Roman <r.hinex@icloud.com>
2024-09-23 01:36:03 +0600

    Change build configuration

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,55 +1,5 @@
-declare module "types/store.type" {
-    import { Locale } from "format-message";
-    export type UIStoreState = {
-        locale: Locale | null;
-    };
-}
-declare module "store" {
-    import { Store } from 'pullstate';
-    import { UIStoreState } from "types/store.type";
-    export const UIStore: Store<UIStoreState>;
-}
-declare module "types/index.type" {
-    import * as formatMessage from 'format-message';
-    import type { Locale, Translations } from 'format-message';
-    export type LanguageMeta = {
-        label: Locale;
-        [key: string]: string | number;
-    };
-    export type LanguagePack = {
-        locale: Locale;
-        meta: LanguageMeta;
-        translations?: Translations;
-    };
-    export type LocaleOptions = {
-        languages: LanguagePack[];
-        localeFromPhone: Locale | null;
-        defaultFallback: Locale | null;
-        locale: Locale | null;
-        translations: {
-            [key: Locale]: Translations;
-        };
-    };
-    export type InitializationParams = {
-        localeFromPhone: () => Locale;
-        languages: LanguagePack[];
-        defaultFallback?: Locale | null;
-    };
-    export type ChangeLocaleFunction = (locale: Locale) => Locale | null;
-    export type InitializationFunction = (params: InitializationParams) => Locale | null;
-    export interface UseIntlReturn {
-        getLanguages: () => (LanguagePack & {
-            selected: boolean;
-        })[];
-        locale: string;
-        t: typeof formatMessage;
-        changeLocale: (locale: Locale) => Locale | null;
-    }
-}
-declare module "index" {
-    import { ChangeLocaleFunction, InitializationFunction, UseIntlReturn } from "types/index.type";
-    export const changeLocale: ChangeLocaleFunction;
-    export const initialization: InitializationFunction;
-    export const useIntl: () => UseIntlReturn;
-}
+import { ChangeLocaleFunction, InitializationFunction, UseIntlReturn } from "./types/index.type";
+export declare const changeLocale: ChangeLocaleFunction;
+export declare const initialization: InitializationFunction;
+export declare const useIntl: () => UseIntlReturn;
 //# sourceMappingURL=index.d.ts.map
---
+++ dist/types/store.type.d.ts
@@ -0,0 +1,5 @@
+import { Locale } from "format-message";
+export type UIStoreState = {
+    locale: Locale | null;
+};
+//# sourceMappingURL=store.type.d.ts.map

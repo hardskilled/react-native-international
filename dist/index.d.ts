29c9e0d2ae65a8b16709b92d5dd7cff902a9affd
Roman <r.hinex@icloud.com>
2024-09-23 03:22:11 +0600

    Remove pullstate

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+import { ChangeLocaleFunction, InitializationFunction, UseIntlReturn } from "./types/index.type";
+export declare const changeLocale: ChangeLocaleFunction;
+export declare const initialization: InitializationFunction;
+export declare const useIntl: () => UseIntlReturn;
+//# sourceMappingURL=index.d.ts.map
